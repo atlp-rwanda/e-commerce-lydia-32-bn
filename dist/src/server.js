0e4fd2ff961b28870ba20e475a7bdfe296d1fa74
kirengamartial <kirengamartial@gmail.com>
2024-05-09 18:03:15 +0200

    E-commerce setup

--- dist/src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/app.ts
-const myVariable = 'hello world';
-function myFunction(arg1, arg2) {
-    console.log(arg1);
-    return arg2; // This should trigger a 'no-unused-vars' ESLint error
-}
-myFunction('hello', 42);
