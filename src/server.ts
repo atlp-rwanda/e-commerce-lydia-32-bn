3dfd589d46ae497917a369e220f9b8108821b18b
kirengamartial <kirengamartial@gmail.com>
2024-05-09 16:54:10 +0200

    Test commit -m

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 // src/app.ts
 const myVariable = 'hello world';
 
+
+
 function myFunction(arg1: string, arg2: number) {
   console.log(arg1);
   return arg2; // This should trigger a 'no-unused-vars' ESLint error
 }
+
 
 
 
