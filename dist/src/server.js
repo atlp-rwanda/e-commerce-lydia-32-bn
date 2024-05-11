26c274cbc331c2295868e9b4601e38a372de51cf
kirengamartial <kirengamartial@gmail.com>
2024-05-11 22:44:04 +0200

    resolve confict

---
+++ dist/src/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+const port = 5000;
+app.get('/', (req, res) => {
+    res.send('welcome to our project');
+});
+app.listen(port, () => {
+    console.log(`app is running on ${port}`);
+});
