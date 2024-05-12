e95c8e7d51a5cd9deaa41c56d8d9483345b16de1
balinda21 <balindamoris.com>
2024-05-12 23:38:26 +0200

    databaseSetup

--- dist/src/config/db.js
+++ dist/src/config/db.js
@@ -1,8 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('smsksjiz', 'smsksjiz', 'f1fOPcWCQhe3EjannsJ9qxi7AEN2qwAR', {
-    host: 'kala.db.elephantsql.com',
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const db = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'postgres'
 });
 exports.default = db;
