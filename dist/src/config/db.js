c62aa5a69b8614a8798d74d2af8640cea0328dc9
balinda21 <balindamoris.com>
2024-05-13 19:27:34 +0200

    Database Setup

--- dist/src/config/db.js
+++ dist/src/config/db.js
@@ -1,13 +1,8 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const db = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres'
 });
-exports.default = db;
+export default db;
