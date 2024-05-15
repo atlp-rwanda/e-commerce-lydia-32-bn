56c3dbe267be63235c15d96cd7a3f2ecb7d66608
@Robs_dagreat <robsrwibutso12@gmail.com>
2024-05-15 12:42:34 +0200

    [200~Update server.ts for unit testing setup

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
-import express from "express";
-import db from "./config/db.js";
-import User from "./models/userModel.js";
-import { Request, Response } from "express";
-import swaggerDocs from "./utilis/swagger.js";
+import express, { Request, Response } from 'express';
+import db from './config/db.js';
+import User from './models/userModel.js';
+
+import swaggerDocs from './utilis/swagger.js';
 
 db.authenticate()
-.then((res) => console.log(`connected to database successfully`))
-.catch((error) => console.log(error))
-const app =express()
-const port = 5000
+  .then((res) => console.log('connected to database successfully'))
+  .catch((error) => console.log(error));
+const app = express();
+const port = 5000;
 
-app.get('/',(req, res)=>{
-    res.send('welcome to our project')
-})
+app.get('/', (req, res) => {
+  res.send('welcome to our project');
+});
 
 swaggerDocs(app, port);
-app.listen(port,()=>{
-    console.log(`app is running on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`app is running on ${port}`);
+});
