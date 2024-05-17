1f3e0a5f127d49e146e8528f170d8e827dbe8454
balinda21 <balindamoris.com>
2024-05-17 14:57:10 +0200

    Resolve conflicts

--- migrations/migration.cjs
+++ migrations/migration.cjs
@@ -4,48 +4,48 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Users', {
       firstname: {
-        type: new DataTypes.STRING(128),
+        type: new Sequelize.STRING(128),
         allowNull: false,
       },
       othername: {
-        type: new DataTypes.STRING(128),
+        type: new Sequelize.STRING(128),
         allowNull: false,
       },
       email: {
-        type: new DataTypes.STRING(128),
+        type: new Sequelize.STRING(128),
         allowNull: false,
       },
       phone: {
-        type: new DataTypes.STRING(128),
+        type: new Sequelize.STRING(128),
         allowNull: false,
       },
       password: {
-        type: new DataTypes.STRING(128),
+        type: new Sequelize.STRING(128),
         allowNull: false,
       },
       usertype: {
-        type: new DataTypes.STRING(128),
+        type: new Sequelize.STRING(128),
         allowNull: false,
       },
       street: {
-        type: new DataTypes.STRING(128),
+        type: new Sequelize.STRING(128),
       },
       city: {
-        type: new DataTypes.STRING(128),
+        type: new Sequelize.STRING(128),
   
       },
       state: {
-        type: new DataTypes.STRING(128),
+        type: new Sequelize.STRING(128),
       }, 
       postal_code: {
-        type: new DataTypes.STRING(128),
+        type: new Sequelize.STRING(128),
       },
       country: {
-        type: new DataTypes.STRING(128),
+        type: new Sequelize.STRING(128),
   
       },
       isAdmin: {
-        type: DataTypes.BOOLEAN,
+        type: Sequelize.BOOLEAN,
         defaultValue: false
       },
     });
