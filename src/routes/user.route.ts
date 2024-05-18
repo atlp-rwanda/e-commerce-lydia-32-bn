398ea4c4a229adfaa5485a82beb14581f9df5f4d
kirengamartial <kirengamartial@gmail.com>
2024-05-18 11:03:19 +0200

    ft: Implement Google login functionality

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import verifyToken from '../middleware/verfication.middleware.js';
 import { UserController } from '../controllers/registeruser.controller.js';
 import {login} from '../controllers/loginUser.js';
+import {loginByGoogle} from '../controllers/LoginUserByEmail.controller.js'
 
 export const usersRouter = express.Router();
 
@@ -78,4 +79,5 @@ usersRouter.get('/users', UserController.getAllUsers);
 usersRouter.put('/users/update//:id', UserController.updateUser);
 usersRouter.delete('/users/delete/:id', UserController.deleteUser);
 usersRouter.post('/login/user', login);
+usersRouter.post('/login',loginByGoogle)
 
