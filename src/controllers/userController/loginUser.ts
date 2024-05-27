e962723b4c513472308f6b9b493cdf61c1a321d3
Derrick [Nuby] Iradukunda <72933285+Derrick-Nuby@users.noreply.github.com>
2024-05-27 10:04:26 +0200

    Changed the Jwt token to the one which was already in use

--- src/controllers/userController/loginUser.ts
+++ src/controllers/userController/loginUser.ts
@@ -6,11 +6,11 @@ import generateToken from '../../utilis/generateToken.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-if (!process.env.JWT_SECRET) {
-    throw new Error('Missing JWT_SECRET environment variable');
+if (!process.env.VERIFICATION_JWT_SECRET) {
+    throw new Error('Missing VERIFICATION_JWT_SECRET environment variable');
 }
 
-const JWT_SECRET: string = process.env.JWT_SECRET;
+const JWT_SECRET: string = process.env.VERIFICATION_JWT_SECRET;
 
 export const login = async (req: Request, res: Response): Promise<void> => {
   try {
