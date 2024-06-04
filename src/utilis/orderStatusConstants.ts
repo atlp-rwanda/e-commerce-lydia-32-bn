e13852ead95f3de363a77d8d172834ac6e6cd9ed
@Robs_dagreat <robsrwibutso12@gmail.com>
2024-06-04 16:43:31 +0200

    feat(track order status): Have a buyer be able to track his/her order status

---
+++ src/utilis/orderStatusConstants.ts
@@ -0,0 +1,16 @@
+export const ORDER_STATUS = {
+    PENDING: 'pending',
+    AWAITING_PAYMENT: 'Awaiting Payment',
+    AWAITING_FULLFILLMENT: 'Awaiting Fullfillment',
+    AWAITING_SHIPMENT: 'Awaiting Shipment',
+    AWAITING_PICKUP: 'Awaiting Pickup',
+    PARTIALLY_SHIPPED: 'Partially Shipped',
+    COMPLETED: 'Complited', 
+    SHIPPED: 'shipped',
+    CANCELED: 'canceled',
+    DECLINED: 'Declined',
+    REFUNDED: 'Refunded',
+    DISPUTED: 'Disputed',
+    MANUAL_VERIFICATION_REQUIRED: 'Manual Verification Required',
+    PARTIALLY_REFUNDED: 'Partially Refunded'
+  };
