8a03b9d839804c073d37acac8a55f1d65caef112
balinda21 <balindamoris@gmail.com>
2024-06-05 08:46:05 +0200

    Processing order

--- src/controllers/orderController.ts/checkoutController.ts
+++ src/controllers/orderController.ts/checkoutController.ts
@@ -1,16 +1,12 @@
-// orderController.ts
 import { Request, Response } from 'express';
 import * as orderService from '../../services/orderService/orderService.js';
 import { AuthenticatedRequest } from '../../middleware/authMiddleware.js';
-import {addToOrder} from '../../services/orderService/orderService.js'
-import User from '../../models/userModel.js';
 
 export const checkout = async (req: AuthenticatedRequest, res: Response) => {
-  const { payment, address, email } = req.body;
-  const currentUser =  await User.findOne({where: {email}})
+  const { payment, address } = req.body;
+  const currentUser = req.user; 
 
   try {
-  
     const newOrder = await orderService.addToOrder(currentUser, payment, address);
 
     return res.status(201).json({ message: 'Order processed successfully', order: newOrder });
--- src/services/orderService/orderService.ts
+++ src/services/orderService/orderService.ts
@@ -6,28 +6,23 @@ import Product from '../../models/productModel.js';
 import User from '../../models/userModel.js';
 import { UserAttributes } from '../../models/userModel.js';
 
-
-
 export const addToOrder = async (currentUser: any, payment: any, address: any) => {
   try {
-console.log('my user id is', currentUser.dataValues.id)
+    console.log('my user id is', currentUser.id); 
     let cart: any = await Cart.findOne({
-
-      //@ts-ignore
-      where: { userId: currentUser.dataValues.id },
-    
+      where: { userId: currentUser.id },
     });
-console.log('my cart total is', cart.dataValues.total)
+
+    console.log('my cart total is', cart.dataValues.total);
     const order = await Order.create({
-      userId: currentUser.dataValues.id,
+      userId: currentUser.id,
       totalAmount: cart.dataValues.total,
       status: "pending",
       payment: payment,
       address: address
-    })
- return order
+    });
 
-
+    return order;
   } catch (error: any) {
     console.error('Error from add to cart:', error.message);
     throw new Error(error.message);
