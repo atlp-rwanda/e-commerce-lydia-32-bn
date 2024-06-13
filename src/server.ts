a086f39fbad0b574c24cb898ea446dd1ee6047da
balinda21 <balindamoris@gmail.com>
2024-06-13 12:10:41 +0200

    Added deleted files

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import http from 'http';
+import { Server } from 'socket.io';
 import db from './config/db.js';
 import swaggerDocs from './utilis/swagger.js';
 import { usersRouter } from './routes/user.route.js';
@@ -9,64 +11,78 @@ import { productRouter } from './routes/productRoutes.js';
 import { sellerRouter } from './routes/sellerRoutes.js';
 import { rolesRouter } from './routes/roleRoutes.js';
 import { wishListRouter } from './routes/wishListRoutes.js';
-import {reviewRouter} from './routes/reviewroute.js'
-
-
+import { notificationRouter } from './routes/notificationRoute.js';
+import { reviewRouter } from './routes/reviewroute.js';
+import { paymentRouter } from './routes/paymentsRoutes.js';
 import cartRoutes from './routes/cartRoutes.js';
-
+import orderRoutes from './routes/orderRoute.js'
+import postRoutes from './routes/postRoutes.js';
+import './cronjobs/expiredProductsCron.js'
+import {startCronJob} from '../src/Jobs/passwordExpirationJob.js'
 
 
 dotenv.config();
 
 db.authenticate()
   .then((res) => console.log('connected to database successfully'))
-  .then((res) => console.log('connected to database successfully'))
-  .then((res) => console.log('connected to database successfully'))
   .catch((error) => console.log(error));
+
 const app = express();
 
-// Use cookie-parser middleware
-// Use cookie-parser middleware
-// Use cookie-parser middleware
-app.use(cors({
-  origin:'*',
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
 
 app.use(cookieParser());
-
 app.use(express.json());
+
+const server = http.createServer(app);
+export const io = new Server(server);
+export const socket = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+setTimeout(() => {
+  socket.emit('orderStatusUpdate', {
+    orderId: '12345',
+    orderStatus: 'Awaiting Payment',
+  });
+}, 5000);
+
+
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.get('/', (req, res) => {
   res.send('welcome to our project');
 });
 
-// Routes for the endpoints
-
-
-
-
-
-
-
-
-
-
-
-
-app.use('/api', usersRouter, productRouter, sellerRouter, rolesRouter);
-app.use('/api', usersRouter, productRouter, sellerRouter, wishListRouter);
-
-app.use('/api', usersRouter,productRouter, sellerRouter,cartRoutes,reviewRouter);
-
+startCronJob();
+app.use(
+  '/api',
+  cartRoutes,
+  notificationRouter,
+  orderRoutes,
+  productRouter,
+  reviewRouter,
+  rolesRouter,
+  sellerRouter,
+  usersRouter,
+  wishListRouter,
+  paymentRouter,
+  postRoutes,
+);
 
 swaggerDocs(app, port);
 app.listen(port, () => {
   console.log(`app is running on http://localhost:${port}`);
 });
-
-
-
-
