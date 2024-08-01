1b3d346dd42e14a93251d523f7861166c72687aa
Ishimwe Pacifique <ishimweinstein@gmail.com>
2024-08-01 13:13:02 +0200

    removed isBuyer middleware

--- src/routes/paymentsRoutes.ts
+++ src/routes/paymentsRoutes.ts
@@ -96,7 +96,6 @@ paymentRouter.get(
   '/payment/success/:sessionId/:orderId',
   isPasswordNotExpired,
   userAuthJWT,
-  isBuyer,
   PaymentController.paymentSuccess,
 );
 
@@ -142,7 +141,6 @@ paymentRouter.get(
   '/payment/cancel/:sessionId/:orderId',
   isPasswordNotExpired,
   userAuthJWT,
-  isBuyer,
   PaymentController.paymentCancel,
 );
 
